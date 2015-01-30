2f3930a598d51cbe257907b0d5257e17ae37193d
Nico Schlömer <nico.schloemer@gmail.com>
2015-01-30 11:54:18 +0100

    initial commit

---
+++ automake-clean.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -ue
+
+for dir in amesos aristos claps CTrilinos isorropia fei ForTrilinos mesquite \
+  moertel moocho new_package pamgen phdmesh rythmos thyra ThreadPool \
+  trilinoscouplings TriKota WebTrilinos zoltan; do
+  echo "Cleaning folder $dir from automake remainders..."
+  rm -rf \
+    $dir/aclocal.m4 \
+    $dir/bootstrap \
+    $dir/bootstrap-local \
+    $dir/configure \
+    $dir/configure.ac \
+    $dir/Makefile.am \
+    $dir/Makefile.in \
+    $dir/Makefile.export.* \
+    $dir/config/
+  echo "done."
+done
+
+
