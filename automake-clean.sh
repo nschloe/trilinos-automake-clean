d74395dc3b689d456b2ed381b96c0c7aeb09bfa5
Nico Schlömer <nico.schloemer@gmail.com>
2015-03-25 19:15:20 +0100

    always only clean current directory

--- automake-clean.sh
+++ automake-clean.sh
@@ -1,20 +1,19 @@
 #!/bin/sh -ue
 
-for dir in amesos aristos claps CTrilinos isorropia fei ForTrilinos mesquite \
-  moertel moocho new_package pamgen phdmesh rythmos thyra ThreadPool \
-  trilinoscouplings TriKota WebTrilinos; do
-  echo "Cleaning folder $dir from automake remainders..."
-  rm -rf \
-    $dir/aclocal.m4 \
-    $dir/bootstrap \
-    $dir/bootstrap-local \
-    $dir/configure \
-    $dir/configure.ac \
-    $dir/Makefile.am \
-    $dir/Makefile.in \
-    $dir/Makefile.export.* \
-    $dir/config/
-  echo "done."
-done
-
-
+#for dir in amesos aristos claps CTrilinos isorropia fei ForTrilinos mesquite \
+#  moertel moocho new_package pamgen phdmesh rythmos thyra ThreadPool \
+#  trilinoscouplings TriKota WebTrilinos; do
+$dir = .
+echo "Cleaning folder $dir from automake remainders..."
+rm -rf \
+  $dir/aclocal.m4 \
+  $dir/bootstrap \
+  $dir/bootstrap-local \
+  $dir/configure \
+  $dir/configure.ac \
+  $dir/Makefile.am \
+  $dir/Makefile.in \
+  $dir/Makefile.export.* \
+  $dir/config/
+echo "done."
+#done
